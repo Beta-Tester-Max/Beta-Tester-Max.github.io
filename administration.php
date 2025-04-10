a464daac23c9a5cf5b064e872dd953c76f748a86
Fea2127 <mingualynx@gmail.com>
2025-04-10 13:12:32 +0800

    admin add tab icon

--- administration.php
+++ administration.php
@@ -9,6 +9,7 @@ if ($_SESSION['authority'] === "Admin") {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Admin Only</title>
+        <link rel="icon" href="./img/aics-logo.ico" type="image/x-icon">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-DQvkBjpPgn7RC31MCQoOeC9TI2kdqa4+BSgNMNj8v77fdC77Kj5zpWFTJaaAoMbC" crossorigin="anonymous">
     </head>
